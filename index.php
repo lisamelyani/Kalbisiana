ee6eebb0822ba49f071021b8fdc0e0a5d8587936
lisamelyani <lisamelyani@gmail.com>
2014-11-26 18:17:12 +0700

    file index

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+	<head>
+	head
+	</head>
+	<body style="margin:0px;">
+		<div><!-- outer -->
+			<div style="background:#99ff33; width:100%; height:150px;">
+				<table border="1">
+					<tr>
+						<td colspan="3" style="width:1260px;">
+							logo
+						</td>
+						<td style="width:50px;">
+							t
+						</td>
+						<td style="width:50px;">
+							f
+						</td>
+					</tr>
+					<tr>
+						<td style="width: 500px;">
+							Student Activities
+						</td>
+						<td style="width: 500px;">
+							 Articles
+						</td>
+						<td style="width: 500px;">
+							Kalbisiana
+						</td>
+					</tr>
+				</table>
+			</div>
+			<div>
+			<table>
+				<tr>
+					<td style="width:50px;">
+						search
+					</td>
+				</tr>
+				<tr>
+					<td style="100px";>
+						tanggal
+					</td>
+				</tr>
+			</table>
+			</div>
+			</div>
+		</div>
+=======
+		<title></title>
+	</head>
+	<body>
+	ada
+>>>>>>> origin/master
+	</body>
+</html>
